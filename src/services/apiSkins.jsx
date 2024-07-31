d99a62f06204ad0bb16c096c5d79733eda88eba6
Gustavo Santos <133147233+GsantosBr12@users.noreply.github.com>
2024-07-31 07:43:09 -0300

    Add files via upload

--- src/services/apiSkins.jsx
+++ src/services/apiSkins.jsx
@@ -1,7 +1,7 @@
 export const fetchSkins = async () => {
 
 
-  const response = await fetch('/src/skins.json');
+  const response = await fetch('./skins.json');
   const data = await response.json();
   return data;
 
